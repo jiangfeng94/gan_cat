53dd071c64dad57c3c7526851b75e10af5a4742f
salafeng <675143618@qq.com>
2017-12-25 20:05:33 +0800

    添加config文件

---
+++ config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 25 14:46:33 2017
+
+@author: SalaFeng-
+"""
+
+epoch =100
+batch_size =16
+img_size =64 * 64*3
+z_size = 100
+h1_size =150
+h2_size =350
+
